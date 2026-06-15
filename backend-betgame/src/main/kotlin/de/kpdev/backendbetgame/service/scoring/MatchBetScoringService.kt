22458e7043bfb229e78098d44e642be964d3bba1
Katharina Reiner <katharina.reiner@hof-university.de>
2026-06-15 20:55:40 +0200

    feat: own scoring for match bets

--- backend-betgame/src/main/kotlin/de/kpdev/backendbetgame/service/scoring/MatchBetScoringService.kt
+++ backend-betgame/src/main/kotlin/de/kpdev/backendbetgame/service/scoring/MatchBetScoringService.kt
@@ -1,5 +1,6 @@
 package de.kpdev.backendbetgame.service.scoring
 
+import de.kpdev.backendbetgame.model.CompetitionStage
 import de.kpdev.backendbetgame.model.Match
 import de.kpdev.backendbetgame.model.MatchBet
 import de.kpdev.backendbetgame.repository.MatchBetRepository
@@ -17,17 +18,24 @@ class MatchBetScoringService(
         }
     }
 
-    // TODO: implement own scoring
-    private fun calculatePoints(bet: MatchBet, match: Match): Int {
-        val exact = bet.predictedHomeGoals == match.homeGoals &&  bet.predictedAwayGoals == match.awayGoals
 
-        if (exact) return 3
+    private fun calculatePoints(bet: MatchBet, match: Match): Int {
+        if(match.awayGoals == null || match.homeGoals == null)
+            return 0
+
+        val correctDuration = if ((match.stage != CompetitionStage.GROUP_STAGE)
+                && (bet.predictedDuration == match.duration)) 1 else 0
+
+        val exact = bet.predictedHomeGoals == match.homeGoals &&  bet.predictedAwayGoals == match.awayGoals
+        if (exact) return 3 + correctDuration
+
+        val proportion = match.homeGoals!! - match.awayGoals!! == bet.predictedHomeGoals - bet.predictedAwayGoals
+        if(proportion) return 2 + correctDuration
 
         val correctOutcome =
             (bet.predictedHomeGoals > bet.predictedAwayGoals && match.homeGoals!! > match.awayGoals!!) ||
-            (bet.predictedHomeGoals < bet.predictedAwayGoals &&  match.homeGoals!! < match.awayGoals!!) ||
-            (bet.predictedHomeGoals == bet.predictedAwayGoals && match.homeGoals == match.awayGoals)
-        if (correctOutcome) return 1
+            (bet.predictedHomeGoals < bet.predictedAwayGoals &&  match.homeGoals!! < match.awayGoals!!)
+        if (correctOutcome) return 1 + correctDuration
 
         return 0
     }
